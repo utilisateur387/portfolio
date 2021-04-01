5eb496e15ac598640e8caea132fd589a47559516
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-04-01 13:41:49 +0200

    Create layout for projects

--- config.rb
+++ config.rb
@@ -12,6 +12,7 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/projects/*', layout: "project_layout"
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
