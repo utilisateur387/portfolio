30853668b56d389e088cdcfcff9a262ffeb7f77c
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-04-03 18:07:47 +0200

    updating filters js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,20 +1,18 @@
-// // This is where it all goes :)
-// require("turbolinks").start()
 
-// import { revealFilters } from 'components/filters';
-
-// document.addEventListener('turbolinks:load', () => {
-//   // Call your functions here, e.g:
-//   revealFilters();
-
-// });
+const getParams = () => {
+  const params = new URLSearchParams(window.location.search).toString().replace('=', '');
+  const mainContent = document.querySelector('.wrapper-index');
+  if (params != "") {
+    mainContent.style.paddingTop = '40px';
+  }
+}
 
 const revealFilters = () => {
-
+  getParams();
+  const params = new URLSearchParams(window.location.search).toString().replace('=', '');
   window.onscroll = () => {
     const filters = document.getElementById('filters');
-
-    if (window.pageYOffset > 200) {
+    if (window.pageYOffset > 60 && params == "") {
       filters.style.top = "0";
     } else {
       filters.style.top = "-40px";
@@ -22,4 +20,6 @@ const revealFilters = () => {
   }
 }
 
-revealFilters();
+window.onload = () => {
+  revealFilters();
+}
